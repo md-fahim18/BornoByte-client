5e6b5438450aca2454410efb438715dc1cc0494e
Nahian Mugdho <nahianmugdho@gmail.com>
2025-08-17 21:51:53 +0600

    email-gone

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-import { Outlet } from "react-router";
+import { Outlet, useLocation, useNavigation } from "react-router";
 import Navbar from "../shared/Navbar";
 import Footer from "../shared/Footer";
+import Loader from "../shared/Loader";
 const MainLayout = () => {
+   const location = useLocation();
+  const [loading, setLoading] = useState(false);
+
+  
+  useEffect(() => {
+    setLoading(true);
+
+    // ছোট delay দিলে smooth effect হয়
+    const timer = setTimeout(() => setLoading(false), 400);
+
+    return () => clearTimeout(timer);
+  }, [location]);
   return (
     <div
       className="w-full px-4 mx-auto 
@@ -14,7 +27,7 @@ const MainLayout = () => {
                "
     >
       <Navbar />
-      <Outlet></Outlet>
+      {loading ? <Loader /> : <Outlet />}
      <Footer></Footer>
     </div>
   );
